8f3c63c6be113afe27e5a2d9d3b545cbc76cca5b
elijah-afolabi <elijah.afolabi@bison.howard.edu>
2019-11-14 23:35:48 -0500

    First Power Calc

--- project2.s
+++ project2.s
@@ -193,6 +193,15 @@ secondPower:
 	addi $s0, $s0, -1
 	addi $a0, $a0, 1
 	j convertString
+
+firstPower:
+	li $s6, 26
+	mult $s4, $s6
+	mflo $s7
+	add $t7, $t7, $s7
+	addi $s0, $s0, -1
+	addi $a0, $a0, 1
+	j convertString
          
 
 displaySum:
